db1f8453f889253772b6b1b08d0fa6689ad00948
Janio Muianga <janiodrey@gmail.com>
2024-03-11 14:45:21 +0200

    sidebar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ol className="sidebar">
+        <li>
+          <div className="step">Step 1</div>
+          <div className="step-title">Your info</div>
+        </li>
+        <li>
+          <div className="step">Step 2</div>
+          <div className="step-title">Select plan</div>
+        </li>
+        <li>
+          <div className="step">Step 3</div>
+          <div className="step-title">Add-ons</div>
+        </li>
+        <li>
+          <div className="step">Step 4</div>
+          <div className="step-title">Summary</div>
+        </li>
+      </ol>
+      <div>form aqui</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
