f1f7421ae5db2e9428155509d07b996fcefb4d59
Janio Muianga <janiodrey@gmail.com>
2024-03-11 16:23:16 +0200

    styling sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 function App() {
   return (
     <>
-      <ol className="sidebar">
-        <li>
-          <div className="step">Step 1</div>
-          <div className="step-title">Your info</div>
-        </li>
-        <li>
-          <div className="step">Step 2</div>
-          <div className="step-title">Select plan</div>
-        </li>
-        <li>
-          <div className="step">Step 3</div>
-          <div className="step-title">Add-ons</div>
-        </li>
-        <li>
-          <div className="step">Step 4</div>
-          <div className="step-title">Summary</div>
-        </li>
-      </ol>
+      <div className="sidebar">
+        <div className="sidebar-item active">
+          <div className="step-nr">1</div>
+          <div>
+            <div className="step">Step 1</div>
+            <div className="step-title">Your info</div>
+          </div>
+        </div>
+
+        <div className="sidebar-item">
+          <div className="step-nr">2</div>
+          <div>
+            <div className="step">Step 2</div>
+            <div className="step-title">Select plan</div>
+          </div>
+        </div>
+
+        <div className="sidebar-item">
+          <div className="step-nr">3</div>
+          <div>
+            <div className="step">Step 3</div>
+            <div className="step-title">Add-ons</div>
+          </div>
+        </div>
+
+        <div className="sidebar-item">
+          <div className="step-nr">3</div>
+          <div>
+            <div className="step">Step 4</div>
+            <div className="step-title">Summary</div>
+          </div>
+        </div>
+      </div>
       <div>form aqui</div>
     </>
   );
